775d9bfb8c4442d0df0f59698b2df3bacd11b319
Alexander Städing <alexanderstaeding@gmail.com>
2023-03-31 14:31:56 +0200

    Add missing comment in JavaMain

--- src/main/java/contest/JavaMain.java
+++ src/main/java/contest/JavaMain.java
@@ -6,6 +6,7 @@ import java.util.stream.*;
 
 public class JavaMain {
     public static void main(String[] args) {
+        // challenge: sum of all numbers
         final int level = 0;
         for (int i = 1; i <= 2; i++) {
             final int sum = Stream.of(Framework.readInput(level, i).split(" "))
